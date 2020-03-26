92dfb57989af88b566a16c69c3f043ffdbc24ad9
Serhii Tsyhankov <40259977+fruts@users.noreply.github.com>
2020-03-26 20:55:38 +0300

    Edit bubble sort

--- src/main/java/Sorts.java
+++ src/main/java/Sorts.java
@@ -1,9 +1,16 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Sorts {
     public static void BubbleSort(int[] array) {
         boolean isSorted = false;
         int buffer = 0;
+        System.out.println("Bubble sorting start");
         while (!isSorted) {
             isSorted = true;
+            for (int i = 0; i < array.length; i++) {
+                System.out.print(array[i]+" ");
+            }
+            System.out.println("");
             for (int i = 0; i < array.length-1; i++) {
                 if(array[i] > array[i+1]) {
                     isSorted = false;
@@ -13,5 +20,11 @@ public class Sorts {
                 }
             }
         }
+        System.out.println("Bubble sorting end");
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5,4,3,2,1};
+        BubbleSort(array);
     }
 }
